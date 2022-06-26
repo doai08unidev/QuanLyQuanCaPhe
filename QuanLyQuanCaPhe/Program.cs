8b38f378b27d2ad292b14cb1fce7b8b365d328fc
doai08unidev <doai08.unidev@gmail.com>
2022-06-26 11:40:23 +0700

    no message

--- QuanLyQuanCaPhe/Program.cs
+++ QuanLyQuanCaPhe/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyQuanCaPhe
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
